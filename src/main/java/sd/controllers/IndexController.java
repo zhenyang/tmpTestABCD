1b73d83f0702dcaea7b81f0e814b13a0a83f27bc
FENG Zhichao <flankerfc@gmail.com>
2016-01-17 01:59:52 +1100

    First commit

---
+++ src/main/java/sd/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package sd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(Model model) {
+        model.addAttribute("userName", "Feng Zhichao");
+        return "index";
+    }
+}
