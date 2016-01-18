197fb1a43fccc80fb9735eaf76e4e5246fed2845
FENG Zhichao <flankerfc@gmail.com>
2016-01-19 01:03:54 +1100

    Cache dependencies locally. Add unit test and test coverage.

---
+++ src/test/java/test/SmokeTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SmokeTest {
+
+    @Test
+    public void shouldBeWorking() {
+        assertTrue(true);
+    }
+}
